9b4c0fad98a94089dd20bf076571961029c05c16
Carlos Alcala <alcala21@gmail.com>
2014-08-05 12:53:01 -0500

    Modify plot 4

--- plot4.R
+++ plot4.R
@@ -18,20 +18,22 @@ data$Time <- NULL
 
 # Make plot 4
 
-png(filename = "plot4.png", width = 480, height = 480)
+# png(filename = "plot4.png", width = 480, height = 480)
 par(mfrow = c(2,2))
-with(data, plot(datetime, Global_active_power, type = "l", 
-	xlab = "", ylab = "Global Active Power (kilowatts)"))
+with(data, {
+	plot(datetime, Global_active_power, type = "l", 
+		xlab = "", ylab = "Global Active Power (kilowatts)")
 
-with(data, plot(datetime, Voltage, type = "l", col = "black"))
+	plot(datetime, Voltage, type = "l", col = "black")
 
-with(data, plot(datetime, Sub_metering_1, type = "l", col = "black", 
-	ylab = "Energy sub metering"))
-lines(data$datetime, data$Sub_metering_2, type = "l", col = "red")
-lines(data$datetime, data$Sub_metering_3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ), 
-	bty = "n", lty = c(1, 1), 
-	col = c("black", "red", "blue") )
+	plot(datetime, Sub_metering_1, type = "l", col = "black", 
+		ylab = "Energy sub metering")
+	lines(data$datetime, data$Sub_metering_2, type = "l", col = "red")
+	lines(data$datetime, data$Sub_metering_3, type = "l", col = "blue")
+	legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ), 
+		bty = "n", lty = c(1, 1), 
+		col = c("black", "red", "blue") )
 
-with(data, plot(datetime, Global_reactive_power, type = "l", col = "black"))
-dev.off()
+	plot(datetime, Global_reactive_power, type = "l", col = "black")
+})
+# dev.off()
