799486641a9974e1a31398ef1ff1fbe448ef76df
Carlos Alcala <alcala21@gmail.com>
2014-08-05 13:28:01 -0500

    Updated labels in plots

--- plot3.R
+++ plot3.R
@@ -20,7 +20,7 @@ data$Time <- NULL
 
 png(filename = "plot3.png", width = 480, height = 480)
 with(data, plot(datetime, Sub_metering_1, type = "l", col = "black", 
-	ylab = "Energy sub metering"))
+	xlab = "", ylab = "Energy sub metering"))
 lines(data$datetime, data$Sub_metering_2, type = "l", col = "red")
 lines(data$datetime, data$Sub_metering_3, type = "l", col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ), 
--- plot4.R
+++ plot4.R
@@ -27,7 +27,7 @@ with(data, {
 	plot(datetime, Voltage, type = "l", col = "black")
 
 	plot(datetime, Sub_metering_1, type = "l", col = "black", 
-		ylab = "Energy sub metering")
+		xlab = "", ylab = "Energy sub metering")
 	lines(data$datetime, data$Sub_metering_2, type = "l", col = "red")
 	lines(data$datetime, data$Sub_metering_3, type = "l", col = "blue")
 	legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ), 
